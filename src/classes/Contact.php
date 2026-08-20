2523af1e646b72f3b0edea68e977477a192934c0
Cuong <Cuongdc21v7n1>
2026-08-20 16:36:10 +0700

    Cai dat phan trang phia server

--- src/classes/Contact.php
+++ src/classes/Contact.php
@@ -68,7 +68,29 @@ class Contact
 
         return $contacts;
     }
+	public function count(): int
+    {
+        $statement = $this->db->prepare('select count(*) from contacts');
+        $statement->execute();
+        return $statement->fetchColumn();
+    }
 
+    public function paginate(int $offset = 0, int $limit = 10): array
+    {
+        $contacts = [];
+        $statement = $this->db->prepare('select * from contacts limit :limit offset :offset');
+        $statement->bindValue(':offset', $offset, \PDO::PARAM_INT);
+        $statement->bindValue(':limit', $limit, \PDO::PARAM_INT);
+        $statement->execute();
+
+        while ($row = $statement->fetch()) {
+            $contact = new Contact($this->db);
+            $contact->fillFromDbRow($row);
+            $contacts[] = $contact;
+        }
+
+        return $contacts;
+    }
     protected function fillFromDbRow(array $row): Contact
     {
         $this->id = $row['id'];
